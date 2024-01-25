48d3cc4408073a7c75b296c543482f83320b1823
adnan <adnan172203@gmail.com>
2024-01-25 18:17:31 +0600

    added two sum solution

---
+++ 1.array&hasing/3.two-sum.js
@@ -0,0 +1,16 @@
+const twoSum = function (nums, target) {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+
+    map.set(nums[i], i);
+  }
+  return [];
+};
+
+console.log(twoSum([2, 4, 6, 7], 9));

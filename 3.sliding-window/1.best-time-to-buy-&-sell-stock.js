00958af7bc5dda9d1d2cafcff09f1b936f45610e
adnan <adnan172203@gmail.com>
2024-12-11 08:18:24 +0600

    docs: Improve comments in stock trading algorithm
    Add clarifying comments to explain the logic
    behind the algorithm's calculations and
    decision-making process. Enhance code
    readability and understanding.

--- 3.sliding-window/1.best-time-to-buy-&-sell-stock.js
+++ 3.sliding-window/1.best-time-to-buy-&-sell-stock.js
@@ -22,3 +22,14 @@ const maxProfit = (prices) => {
 const prices = [7, 1, 5, 3, 6, 4];
 
 console.log(maxProfit(prices));
+
+// prices[right] - current sell price
+// prices[left] - current buy price
+
+// Inside the loop, we check if the current sell price (prices[right]) is higher than the current buy price (prices[left]).
+
+// else {
+//   left = right;
+// }
+
+// If the current sell price is not higher than the buy price, we update the buy price to the current sell price (left = right). This is because we want to find the lowest price to buy the stock before selling it at a higher price.
